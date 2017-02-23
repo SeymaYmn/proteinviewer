c97908cbe6a65c1ca3613e2d53b1460651df276b
SeymaYmn <seymaaymn@gmail.com>
2017-02-23 17:42:47 +0200

    Add files via upload

--- script/BallAndStick.js
+++ script/BallAndStick.js
@@ -2,6 +2,26 @@ function BallAndStick(protein) {
     this.protein = protein;
 }
 
+BallAndStick.prototype.addBalls = function () {
+
+};
+
+BallAndStick.prototype.addBackboneLines = function () {
+
+};
+
+BallAndStick.prototype.addSideChainLines = function () {
+
+};
+
+BallAndStick.prototype.eachAtom = function (protein) {
+    for(var i=0;i<protein.length;i++){
+        for(var j=0; j<protein.residues[i].atoms.length; j++){
+
+        }
+    }
+};
+
 BallAndStick.prototype.animate = function () {
 
 };
